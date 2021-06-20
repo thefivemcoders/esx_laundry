beb69f5e73569698e8bf9454cff1ffd4fd5057ea
TheFiveMCoders <44032346+thefivemcoders@users.noreply.github.com>
2021-06-20 13:18:24 +0530

    Add files via upload

---
+++ esx_laundry.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('washed_clothes', 'Washed Clothes', 10),
+	('ironed_clothes', 'Ironed Clothes', 10),
+	('packed_clothes', 'Packed Clothes', 10)
+;
