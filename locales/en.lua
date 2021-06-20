3641f844548e1a9d87fb03ec4d61b47dc34179bd
TheFiveMCoders <44032346+thefivemcoders@users.noreply.github.com>
2021-06-20 13:16:40 +0530

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,17 @@
+Locales['en'] = {
+	['carry_for_cloth'] = 'Press ~INPUT_CONTEXT~ to wash clothes',
+	['start_iron_cloth'] = 'Press ~INPUT_CONTEXT~ to iron clothes',
+	['pack_cloth'] = 'Press ~INPUT_CONTEXT~ to pack clothes',
+	['machine_full'] = 'No More Clothes left for washing.',
+	['wash_cloth'] = '~y~washing clothes~s~...',
+	['no_wahsed_cloths'] = 'you dont have wahed clothes with you.',
+	['no_more_wahsed_cloths'] = 'You Do not Have More ~r~clothes~s~s~s~',
+	['wait_iron_cloth'] = '~y~Wait To Iron clothes~s~...',
+	['you_do_not_have_packed_cloth'] = 'You Do not Have More ~r~Packed clothes~s~',
+	['sell_cloth'] = 'Packed ~y~x1~s~ ~y~Ironed Clothes~s~',
+	['sell_packed_cloths'] = '~g~To Pack clothes ~s~...',
+	-- Blips 
+	['wasing_clothes'] = 'Laundry Area',
+	['iron_clothes'] = 'Clothes Ironing Area',
+	['pack_ironed_cloths'] = 'Clothes Packing Area',
+}
